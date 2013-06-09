6ae1da26c011aa4799177bfe64c2715068a049c9
bookworm <han@sangheon.com>
2013-06-09 19:51:10 +0900

    6월 발표 자료 디렉토리 이름 변경

--- 2013-06-04_SPL_Data_Structures/examples/13_heap_max.php
+++ 2013-06-04_SPL_Data_Structures/examples/13_heap_max.php
@@ -1,17 +1,6 @@
 <?php
 class MyMaxHeap extends SplMaxHeap
 {
-    final public function compare($a, $b)
-    {
-        if ($a > $b) {
-            return 1;
-        }
-        if ($a < $b) {
-            return -1;
-        }
-
-        return 0;
-    }
 }
 
 $heap = new MyMaxHeap();
